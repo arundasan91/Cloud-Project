d399c9f11409579c2376f4a87d091dc6fcedc34a
Arun Das <arundas1991@gmail.com>
2016-03-06 14:30:54 -0600

    Update dataset_reduce.R
    fixed row name bug

--- Code/Dataset/dataset_reduce.R
+++ Code/Dataset/dataset_reduce.R
@@ -9,7 +9,7 @@ d_subset = dataset[,c(1,2,3,4,5,6,10,11,12,13,14,15,16)]
 #update header
 colnames(d_subset) <- c("ID","Univ_Name","Major","Degree","Season","Decision","UG_GPA","GRE_Verbal","GRE_Quant","GRE_Writing","Is_new_GRE","GRE_Subject","Status")
 #write the subset to a csv
-write.csv(d_subset, file="datareduced.csv")
+write.csv(d_subset, file="datareduced.csv",row.names = FALSE)
 #write the first 100 entries into data100.csv
 data100 <- d_subset[1:100,]
 write.csv(data100,file="data100.csv",row.names = FALSE)
